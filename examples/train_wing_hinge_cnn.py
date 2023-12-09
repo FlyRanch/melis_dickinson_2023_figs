62de674537dcff3f8fa7d2e0fd98dddedb990ba1
Will Dickson <wbd@caltech.edu>
2023-12-08 16:29:00 -0800

    Corrected the list of outliers and remade the training example.

--- examples/train_wing_hinge_cnn.py
+++ examples/train_wing_hinge_cnn.py
@@ -10,7 +10,18 @@ dataset_dir = curr_dir / 'main_muscle_and_wing_data.h5'
 
 net = WingHingeCNN()
 net.load_dataset(dataset_dir)
-outliers = -np.ones((1,1)) 
+outliers = np.array([
+    [18,0,100],
+    [230,0,1000],
+    [231,0,1000],
+    [232,0,1000],
+    [261,0,50],
+    [338,0,1000],
+    [345,100,150],
+    [346,0,1000],
+    [354,0,120]
+    ])
+
 net.create_dataset(outliers)
 net.train_network(n_filters, curr_dir, save_history=True)
 
